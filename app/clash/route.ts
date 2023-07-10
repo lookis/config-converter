f74904f196637ab5926573bb5f31ba772cecd9c6
Jingsi <lookisliu@gmail.com>
2023-07-10 11:22:16 +0800

    add all proxy

---
+++ app/clash/route.ts
@@ -0,0 +1,107 @@
+
+import rules from '@/rules';
+import yaml from 'js-yaml';
+import { type NextRequest } from 'next/server';
+
+const config_global = `
+mixed-port: 7890
+allow-lan: true
+bind-address: '*'
+mode: rule
+log-level: info
+external-controller: 127.0.0.1:9090
+dns:
+  enable: true
+  ipv6: false
+  default-nameserver:
+    - 223.5.5.5
+    - 119.29.29.29
+  enhanced-mode: fake-ip
+  fake-ip-range: 198.18.0.1/16
+  use-hosts: true
+  nameserver:
+    - https://doh.pub/dns-query
+    - https://dns.alidns.com/dns-query
+  fallback:
+    - https://doh.dns.sb/dns-query
+    - https://dns.cloudflare.com/dns-query
+    - https://dns.twnic.tw/dns-query
+    - tls://8.8.4.4:853
+  fallback-filter:
+    geoip: true
+    ipcidr:
+      - 240.0.0.0/4
+      - 0.0.0.0/32
+`
+
+function generated_proxy(name: string, proxies: string[], type: string = "url-test") {
+  const proxy: any = {
+    "name": name,
+    "type": type,
+    "proxies": proxies,
+  }
+  if (type == "url-test") {
+    proxy['url'] = "http://www.gstatic.com/generate_204"
+    proxy['interval'] = 3600
+  }
+  return proxy
+}
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const token = searchParams.get('token');
+  const config: any = yaml.load(config_global)
+  const us_proxy_names: string[] = []
+  const proxy_names: string[] = []
+  const proxy_groups = []
+  if (token === process.env.TOKEN) {
+    const promises: Promise<any>[] = []
+    for (const [proxy_name, url] of Object.entries(process.env)) {
+      if (url && proxy_name.toLowerCase().startsWith('clash_')) {
+        promises.push(fetch(url, {
+          headers: {
+            "user-agent": "ClashX/1.116.0 (com.west2online.ClashX; build:1.116.0; macOS 13.3.1) Alamofire/5.7.1"
+          }
+        }).then((resp) => {
+          return resp.text();
+        }).then((resp_text) => {
+          const proxy_config: any = yaml.load(resp_text)
+          // add proxy
+          for (const proxy of proxy_config['proxies']) {
+            const name = proxy['name'] as string
+            // for all proxy group
+            proxy_names.push(name)
+            // for us proxy group
+            if (name.includes('美国')) {
+              us_proxy_names.push(name)
+            }
+          }
+          // add this proxy group
+          proxy_groups.push(generated_proxy(proxy_name.replace('CLASH_', ''),
+            proxy_config['proxies'].map((proxy: any) => {
+              return proxy['name']
+            })))
+        }))
+      }
+    }
+    await Promise.all(promises)
+  }
+
+  //global auto proxy
+  proxy_groups.unshift(generated_proxy("AUTO", proxy_names))
+
+  //global proxy: auto, each provider
+  proxy_groups.unshift(generated_proxy("PROXY", proxy_groups.map((proxy_group: any) => {
+    return proxy_group['name']
+  }), 'select'))
+
+  //other
+  proxy_groups.push(generated_proxy("US", us_proxy_names))
+
+  config["proxy-groups"] = proxy_groups
+  config['rules'] = rules()
+  console.log(config)
+  return new Response(yaml.dump(config));
+}
+
+export const dynamic = 'force-dynamic'
--- app/meomiao/clash/route.ts
+++ app/meomiao/clash/route.ts
@@ -1,40 +1,11 @@
 
-import { general, us as us_rules } from '@/rules';
-import yaml from 'js-yaml';
-import { type NextRequest } from 'next/server';
+import { redirect } from 'next/navigation';
+import { NextResponse, type NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, response: NextResponse) {
   const { searchParams } = new URL(request.url);
   const token = searchParams.get('token');
-  const resp = await fetch('https://api.meomiao.me/api/v1/client/subscribe?token=' + token, {
-    headers: {
-      "user-agent": "ClashX/1.116.0 (com.west2online.ClashX; build:1.116.0; macOS 13.3.1) Alamofire/5.7.1"
-    }
-  })
-
-  const config: any = yaml.load(await resp.text())
-  const us = []
-  for (const proxy of config['proxies']) {
-    const name = proxy['name'] as string
-    if (name.includes('美国')) {
-      us.push(name)
-    }
-  }
-
-  if (us.length > 0) {
-    config["proxy-groups"].push({
-      "name": "us",
-      "type": "url-test",
-      "proxies": us,
-      "url": "http://www.gstatic.com/generate_204",
-      "interval": 86400,
-    })
-    config['rules'] = us_rules(config['rules'])
-    config["proxy-groups"][0]['proxies'].splice(2, 0, "us")
-  }
-  config['rules'] = general(config['rules'])
-
-  return new Response(yaml.dump(config));
+  return redirect('/clash?token=' + token)
 }
 
 export const dynamic = 'force-dynamic'
